70881aaac983385c7956a6bfc0d4525ad2239b84
Matin Ghanbari <Matin.Ghanbari@email.kntu.ac.ir>
2023-10-27 20:33:00 +0330

    Update telegram_bot.bot for text messages

--- src/application/telegram_bot/bot.py
+++ src/application/telegram_bot/bot.py
@@ -1,9 +1,8 @@
-import os
-
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, ContextTypes
 
 from src.application.telegram_bot.command_handlers import register_command_handlers
+from src.application.telegram_bot.text_handlers import register_text_handlers
 from src.bootstrapper.service_registrar import ServiceRegistrar
 from src.infrastructure.config import Config
 from src.infrastructure.container import Container
@@ -11,19 +10,6 @@ from src.infrastructure.container import Container
 container = Container()
 ServiceRegistrar(container=container).register_services()
 config = container.resolve(Config)
-
-
-
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: str = update.message.chat.type
-    user_message: str = update.message.text
-
-    print(f'User {update.message.chat.id} in {message_type}: "{user_message}')
-
-    response = "Hello 🙋‍♀️!"
-
-    await update.message.reply_text(response)
 
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -36,8 +22,7 @@ def start_bot():
     app = Application.builder().token(config.api_key).build()
 
     register_command_handlers(app)
-
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    register_text_handlers(app)
 
     app.add_error_handler(error)
 

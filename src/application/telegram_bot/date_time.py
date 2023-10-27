51a91ac4c6a462db3f16f3433b1fc4e276af6b21
Matin Ghanbari <Matin.Ghanbari@email.kntu.ac.ir>
2023-10-27 20:10:19 +0330

    Add date_time.py

---
+++ src/application/telegram_bot/date_time.py
@@ -0,0 +1,12 @@
+# Days List
+WEEK_DAYS = ['شنبه', 'یک‌شنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنج‌شنبه', 'جمعه']
+WEEK_DAYS_DICT = dict(Saturday='شنبه', Sunday='یک‌شنبه', Monday='دوشنبه', Tuesday='سه‌شنبه', Wednesday='چهارشنبه', Thursday='پنج‌شنبه', Friday='جمعه',)
+
+# Day Limit Value
+DAYS_LIMIT = 2
+
+# One Week As Milliseconds
+ONE_WEEK = 7 * 24 * 60 * 60
+
+# One Second As Milliseconds
+ONE_SECONDS = 1_000

6f043b03304cca563800fd670f19a437d74b3a9c
Matin Ghanbari <Matin.Ghanbari@email.kntu.ac.ir>
2023-10-27 20:32:42 +0330

    Update help command handler to reply in markdown mode

--- src/application/telegram_bot/command_handlers.py
+++ src/application/telegram_bot/command_handlers.py
@@ -15,4 +15,4 @@ async def reply_start_command(update: Update, context: ContextTypes.DEFAULT_TYPE
 
 
 async def reply_help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(help)
+    await update.message.reply_text(help, parse_mode=telegram.constants.ParseMode.MARKDOWN)

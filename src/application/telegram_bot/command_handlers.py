7de4f7bd5f8ae6e696fed3d39dac8758510ec64b
Matin Ghanbari <Matin.Ghanbari@email.kntu.ac.ir>
2023-10-27 19:58:45 +0330

    Add command_handlers file

---
+++ src/application/telegram_bot/command_handlers.py
@@ -0,0 +1,18 @@
+import telegram
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes
+
+from src.application.telegram_bot.messages import start_command, help_command, welcome, help
+
+
+def register_command_handlers(app):
+    app.add_handler(CommandHandler(start_command, reply_start_command))
+    app.add_handler(CommandHandler(help_command, reply_help_command))
+
+
+async def reply_start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(welcome, parse_mode=telegram.constants.ParseMode.MARKDOWN)
+
+
+async def reply_help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(help)

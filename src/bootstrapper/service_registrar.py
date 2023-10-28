01660d391637d2f4715ac7671d7e4e31f6716040
Matin Ghanbari <Matin.Ghanbari@email.kntu.ac.ir>
2023-10-29 00:04:08 +0330

    Register db configs

--- src/bootstrapper/service_registrar.py
+++ src/bootstrapper/service_registrar.py
@@ -1,5 +1,5 @@
 from src.data_access.repositories.user_repository import UserRepository
-from src.infrastructure.config import Config
+from src.infrastructure.config import Config, MongoConfig, RedisConfig
 from src.infrastructure.logger import Logger
 
 
@@ -12,3 +12,10 @@ class ServiceRegistrar:
         self.container.register(Config, lambda: Config().load())
         self.container.register(Logger, lambda: Logger("UniversityMealReservation"))
         self.container.register(UserRepository, lambda: UserRepository())
+        mongo_configs = self.container.resolve(Config)['mongo']
+        self.container.register(MongoConfig, lambda: MongoConfig(mongo_configs['host'], mongo_configs['port'],
+                                                                 mongo_configs['username'], mongo_configs['password'],
+                                                                 mongo_configs['database']))
+        redis_configs = self.container.resolve(Config)['redis']
+        self.container.register(RedisConfig, lambda: RedisConfig(redis_configs['host'], redis_configs['port'],
+                                                                 redis_configs['password']))
